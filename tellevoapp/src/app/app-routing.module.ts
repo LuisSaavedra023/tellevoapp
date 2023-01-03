18fd3b1b83047738da199b78d8015fd93e5795c3
Luis Saavedra <l.saavedra.r023@gmail.com>
2023-01-03 02:08:14 -0300

    inicializando, cambio de v node y add angular mat

---
+++ tellevoapp/src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
+    BrowserAnimationsModule
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
